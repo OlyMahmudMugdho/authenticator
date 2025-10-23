9f8059058116a4660207f51216b16c1fcad594ad
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-10-23 13:24:31 +0600

    move page title to center

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,14 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Authenticator' }} />
-      <Stack.Screen name="add" options={{ title: 'Add Account' }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: "Authenticator", headerTitleAlign: "center" }}
+      />
+      <Stack.Screen
+        name="add"
+        options={{ title: "Add Account", headerTitleAlign: "center" }}
+      />
     </Stack>
   );
 }
